761da4f84fde08e83f9ea6a57ffd1c294a6e2f0b
billkondo <willkondo@gmail.com>
2019-05-12 00:13:30 -0300

    Added dependent destroy to multiple choice model

--- app/models/multiple_choice_problem.rb
+++ app/models/multiple_choice_problem.rb
@@ -1,6 +1,6 @@
 class MultipleChoiceProblem < ApplicationRecord
   belongs_to :user
-  has_many :alternatives
+  has_many :alternatives, dependent: :destroy
 
   accepts_nested_attributes_for :alternatives, allow_destroy: true 
 end
