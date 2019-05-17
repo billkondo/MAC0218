6e6d1d355ccc4a9362286cd6fdc8eef04a5e50a0
Gabriel <gabfeol@gmail.com>
2019-05-17 17:04:50 -0300

    Estiliza Home

--- app/javascript/components/home/Home.jsx
+++ app/javascript/components/home/Home.jsx
@@ -3,15 +3,25 @@ import { Link } from 'react-router-dom';
 import { Grid, Typography, Button } from '@material-ui/core';
 
 const Home = () => (
-  <Grid container direction="column" spacing={32}>
-    <Grid item>
-      <Typography variant="h3">Home</Typography>
-    </Grid>
+  <Grid
+    container
+    justify="center"
+    spacing={32}
+    style={{
+      position: 'absolute',
+      top: '10%'
+    }}
+  >
+    <Grid item md={8} xs={12}>
+      <Grid item>
+        <Typography variant="h3">Home</Typography>
+      </Grid>
 
-    <Grid item>
-      <Link to="/create_problem/" style={{ textDecoration: 'none' }}>
-        <Button variant="contained"> Create Problem</Button>
-      </Link>
+      <Grid item mt={5}>
+        <Link to="/create_problem/" style={{ textDecoration: 'none' }}>
+          <Button variant="contained"> Create Problem</Button>
+        </Link>
+      </Grid>
     </Grid>
   </Grid>
 );
