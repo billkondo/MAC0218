22666783268f016b6a2fc7c4f321ac22bedfc61c
billkondo <willkondo@gmail.com>
2019-05-19 21:44:09 -0300

    Hot fix: routing problem due to component name change

--- app/javascript/packs/app.jsx
+++ app/javascript/packs/app.jsx
@@ -14,45 +14,38 @@ import {
 } from '../components';
 import { routes } from '../config';
 
-const App = ({ isAuth }) => (
-  <BrowserRouter>
-    <CssBaseline />
-    <Navbar isAuth={isAuth} />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/create_problem/" component={TypeSelection} />
-      {isAuth ? 
+const App = ({ isAuth }) => {
+  return (
+    <BrowserRouter>
+      <CssBaseline />
+      <Navbar isAuth={isAuth} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/create_problem/" component={TypeSelection} />
+        {isAuth ? (
+          <Route
+            exact
+            path={routes.multiple_choice_form}
+            component={MultipleChoice.Create}
+          />
+        ) : (
+          <Route exact path={routes.multiple_choice_form} component={SignIn} />
+        )}
+        <Route exact path="/sign-in/" component={SignIn} />
+        <Route exact path="/sign-up/" component={SignUp} />
+        <Route exact path={routes.user_profile} component={Profile} />
         <Route
-          exact
-          path={routes.multiple_choice_form}
-          component={MultipleChoice.Form}
+          path="/problems/multiple_choice/:id"
+          component={MultipleChoice.Read}
         />
-        :
         <Route
-          exact
-          path={routes.multiple_choice_form}
-          component={SignIn}
+          path="/edit_problem/multiple_choice/:id"
+          component={MultipleChoice.Edit}
         />
-      }
-      <Route
-        exact
-        path={routes.multiple_choice_form}
-        component={MultipleChoice.Create}
-      />
-      <Route exact path="/sign-in/" component={SignIn} />
-      <Route exact path="/sign-up/" component={SignUp} />
-      <Route exact path={routes.user_profile} component={Profile} />
-      <Route
-        path="/problems/multiple_choice/:id"
-        component={MultipleChoice.Read}
-      />
-      <Route
-        path="/edit_problem/multiple_choice/:id"
-        component={MultipleChoice.Edit}
-      />
-    </Switch>
-  </BrowserRouter>
-);
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 document.addEventListener('DOMContentLoaded', () => {
   const isUserSignedIn =
