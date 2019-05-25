25477196ed60fda7259923c176e37344d681ec0d
billkondo <willkondo@gmail.com>
2019-05-24 21:24:07 -0300

    Hot change: adding validation to multiple choice problem model

--- app/models/multiple_choice_problem.rb
+++ app/models/multiple_choice_problem.rb
@@ -4,4 +4,10 @@ class MultipleChoiceProblem < ApplicationRecord
   has_many :multiple_choice_favorites, dependent: :destroy
 
   accepts_nested_attributes_for :alternatives, allow_destroy: true
+
+  validates :title, presence: true
+  validates :statement, presence: true 
+  validates :correct_answer, presence: true
+
+  validates :alternatives, length: { minimum: 2 }
 end
