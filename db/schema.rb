649c3ed2e758dd12fcab152898307cf23c28db30
billkondo <willkondo@gmail.com>
2019-06-08 15:44:45 -0300

    Merge master

--- db/schema.rb
+++ db/schema.rb
@@ -39,14 +39,6 @@ ActiveRecord::Schema.define(version: 2019_05_23_144034) do
     t.index ["user_id"], name: "index_multiple_choice_problems_on_user_id"
   end
 
-  create_table "problems", force: :cascade do |t|
-    t.string "title"
-    t.string "statement"
-    t.integer "answer"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
