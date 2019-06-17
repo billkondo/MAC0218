71702693d9e45b585a05d19f754b87649a40dbf8
Gabriel <gabfeol@gmail.com>
2019-06-17 17:08:06 -0300

    Tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
     before do
         @user = User.create(username: "usuario", email: "usuario@gmail.com", password: 123456)
-        @user2 = User.create(username: "user2", email: "user1992@gmail.com", password: 123)
+        @user2 = User.create(username: "user2", email: "user1992@gmail.com", password: 12345)
     end
     it "has valid attributes" do
         expect(@user).to be_valid
+        # Passwords must have 6 or more characters
         expect(@user2).not_to be_valid
     end
+    
 end
