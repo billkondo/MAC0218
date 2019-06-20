1a221ad509f81f08e10594f6acba89f4a0532b43
Gabriel <gabfeol@gmail.com>
2019-06-20 18:59:15 -0300

    Testes basicos de controlador

---
+++ spec/controllers/multiple_choice_problems_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe MultipleChoiceProblemsController, type: :controller do
+    describe "Multiple choice problems creation" do
+        it "has 200 status code" do
+           # post :create_multiple_choice, :params => {
+           #             :multiple_choice_problem_params => 
+            #            {
+             #               :title => "Problema do Teste",
+              #              :statement => "Enunciado",
+               #             :correct_answer => "1",
+                #            :alternatives_attributes => {
+                 #               :text => "asddsada"
+                  #          }
+                   #     }}
+        end
+    end
+end
