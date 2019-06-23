1b481db8f54cd0f2fdcdd508905b3847e2839f33
Gabriel <gabfeol@gmail.com>
2019-06-23 19:35:04 -0300

    Passa pra ptbr e remove typo

--- app/javascript/components/home/Home.jsx
+++ app/javascript/components/home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => (
   <Grid container justify="center">
     <Grid item md={8} xs={12}>
       <Grid item>
-        <Typography variant="h3">Home</Typography>
+        <Typography variant="h3">Home Page</Typography>
       </Grid>
 
       <Grid container spacing={4}>
@@ -17,12 +17,12 @@ const Home = () => (
             to={routes.problems.create_select_type}
             style={{ textDecoration: 'none' }}
           >
-            <Button variant="contained"> Create Problem</Button>
+            <Button variant="contained"> Criar Problema</Button>
           </Link>
         </Grid>
         <Grid item mt={5}>
           <Link to="/problems" style={{ textDecoration: 'none' }}>
-            <Button variant="contained"> All Problems</Button>
+            <Button variant="contained"> Todos Problemas</Button>
           </Link>
         </Grid>
       </Grid>
