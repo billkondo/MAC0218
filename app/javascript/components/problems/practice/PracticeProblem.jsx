9729a190195f0a71b8a16a918dc4c852e194711e
Pedro Sousa <pedroteosousa@gmail.com>
2019-06-24 19:45:19 -0300

    Create component for multiple choice practice

--- app/javascript/components/problems/practice/PracticeProblem.jsx
+++ app/javascript/components/problems/practice/PracticeProblem.jsx
@@ -1,29 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { 
-  Grid,
-  Button,
-  FormControl,
-  RadioGroup,
-  Radio,
-  FormControlLabel,
-  FormLabel,
-} from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 
-import Alternative from '../multiple_choice/form/alternatives/Alternative'
-
-import { withRouter } from 'react-router-dom';
+import PracticeMultipleChoice from './PracticeMultipleChoice'
 
 import { Services } from '../../../services';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-  },
-  group: {
-    margin: theme.spacing(1, 0),
-  },
-}));
 
 const STATUS = {
   DONE: 'DONE',
@@ -31,91 +10,55 @@ const STATUS = {
   ERROR: 'ERROR'
 };
 
-export const PracticeProblem = ({ match }) => {
-  const classes = useStyles()
+const TYPE = {
+  MULTIPLE_CHOICE: 'MULTIPLE_CHOICE',
+  WRITE: 'WRITE',
+}
 
+export const PracticeProblem = ({ match, id }) => {
   const [problem, setProblem] = useState({});
   const [alternatives, setAlternatives] = useState([]);
+  const [type, setType] = useState('')
   const [status, setStatus] = useState(STATUS.LOADING);
-  const [marked, mark] = useState('')
-  const [solved, setSolved] = useState(false)
-
 
   useEffect(() => {
-    const id = match.params.id;
+    const problem_id = id || match.params.id;
 
-    Services.Api.MultipleChoice.get_multiple_choice_problem(id)
+    Services.Api.MultipleChoice.get_multiple_choice_problem(problem_id)
       .then(res => {
-        const { problem, alternatives } = res.data;
-
-        if (res.data.status === 'OK') {
-          setAlternatives(alternatives);
-          setProblem(problem);
-          setStatus(STATUS.DONE);
-        } else {
-          setStatus(STATUS.ERROR);
+        if(res.data.status != "ERROR"){
+          const { problem, alternatives } = res.data;
+          try {
+            setProblem(problem)
+            setAlternatives(alternatives)
+            setType(TYPE.MULTIPLE_CHOICE)
+            setStatus(STATUS.DONE)
+          } catch(e) {
+            console.error(e)
+          }
         }
       })
-      .catch(err => console.log(err));
+      .catch(_ => this.setState({ status: this.status.error }));
+
+    Services.Api.write.get(problem_id)
+      .then(res => {
+        if (res.data.status != "ERROR") {
+          const problem = res.data.write_problem;
+          try {
+            setProblem(problem)
+            setType(TYPE.WRITE)
+            setStatus(STATUS.DONE)
+          } catch (e) {
+            console.error(e);
+          }
+        }
+      })
   }, []);
 
   return status === STATUS.DONE ? (
-    <Grid container alignContent="center" style={{ paddingLeft: 32, paddingRight: 32 }}>
-      <Grid container>
-        <h2>{problem.statement}</h2>
-      </Grid>
-      <Grid container>
-        <FormControl fullWidth component="fieldset">
-          <FormLabel component="legend">Alternatives</FormLabel>
-          <RadioGroup
-            aria-label="Alternatives"
-            name="alternatives"
-            className={classes.group}
-            value={marked}
-            onChange={event => {
-              if (!solved) {
-                mark(event.target.value)
-              }
-            }}
-          >
-            {
-              alternatives.map(alternative => {
-                const isCorrect = alternative.text === problem.correct_answer
-                const isMarked = alternative.id.toString() === marked 
-
-                return (
-                  <FormControlLabel
-                    value={alternative.id.toString()}
-                    control={<Radio color="primary" />}
-                    label={
-                      !solved
-                        ? alternative.text
-                        :
-                        <Alternative
-                          alt={alternative}
-                          mode="read"
-                          isCorrect={isCorrect}
-                          isWrong={isMarked && !isCorrect}
-                        />
-                    }
-                    key={alternative.id}
-                  />)
-              })
-            }
-          </RadioGroup>
-        </FormControl>
-      </Grid>
-      {!solved ?
-        <Grid item container spacing={2} justify="flex-end">
-          <Grid item>
-            <Button variant="outlined" onClick={() => {
-              setSolved(true)
-            }}>
-              Submit
-            </Button>
-          </Grid>
-        </Grid>
-        : null}
-    </Grid>
+    <PracticeMultipleChoice
+      problem={problem}
+      alternatives={alternatives}
+    />
   ) : null;
 };
