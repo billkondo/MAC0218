6b0b279ecb3e1f7a217df164e3c70cc8ef0ba536
Gabriel <gabfeol@gmail.com>
2019-06-24 23:09:43 -0300

    db config

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_24_004945) do
+ActiveRecord::Schema.define(version: 2019_06_24_160626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -25,16 +25,6 @@ ActiveRecord::Schema.define(version: 2019_06_24_004945) do
     t.index ["multiple_choice_problem_id"], name: "index_alternatives_on_multiple_choice_problem_id"
   end
 
-  create_table "mocks", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.uuid "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.text "problems", default: [], array: true
-    t.index ["user_id"], name: "index_mocks_on_user_id"
-  end
-  
   create_table "blogs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "title"
     t.string "sub_title"
@@ -51,6 +41,16 @@ ActiveRecord::Schema.define(version: 2019_06_24_004945) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "mocks", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.uuid "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "problems", default: [], array: true
+    t.index ["user_id"], name: "index_mocks_on_user_id"
   end
 
   create_table "multiple_choice_favorites", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -103,8 +103,8 @@ ActiveRecord::Schema.define(version: 2019_06_24_004945) do
   end
 
   add_foreign_key "alternatives", "multiple_choice_problems"
-  add_foreign_key "mocks", "users"
   add_foreign_key "blogs", "groups"
+  add_foreign_key "mocks", "users"
   add_foreign_key "multiple_choice_favorites", "multiple_choice_problems"
   add_foreign_key "multiple_choice_favorites", "users"
   add_foreign_key "write_problem_questions", "write_problems"
