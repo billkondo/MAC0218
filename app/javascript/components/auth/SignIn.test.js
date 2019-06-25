feda649689e148c4b33386dd76530407e2785ca8
Gabriel <gabfeol@gmail.com>
2019-06-25 11:45:47 -0300

    Fix Jest test

--- app/javascript/components/auth/SignIn.test.js
+++ app/javascript/components/auth/SignIn.test.js
@@ -3,10 +3,11 @@ import SignIn from './SignIn'
 import { mount, configure } from 'enzyme'
 import toJson from "enzyme-to-json"
 import Adapter from 'enzyme-adapter-react-16'
+import { BrowserRouter as Router } from 'react-router-dom';
 
 configure({adapter: new Adapter()});
 it("should render the Sign In webpage", () => {
-  const app = mount(<SignIn />)
+  const app = mount(<Router> <SignIn /> </Router>)
   expect(toJson(app)).toMatchSnapshot()
   app.unmount
 })
